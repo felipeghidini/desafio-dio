ccb7b5c629c73be415b123628ef2c6e0a1533ac9
felipe.ghidini <felipe.ghidini@contabilizei.com.br>
2024-07-11 17:19:25 -0300

    first commit

---
+++ ContaTerminal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta");
+    }
+}
