a09b80fe4025de3a502d7825703ff3d37ca82025
felipe.ghidini <felipe.ghidini@contabilizei.com.br>
2024-07-11 17:25:39 -0300

    Projeto finalizado

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -8,5 +8,20 @@ public class ContaTerminal {
         String agencia = sc.nextLine();
 
         System.out.println("Por favor, digite o número da Conta");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = sc.nextDouble();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+
+        // Fechando o scanner
+        sc.close();
     }
 }
